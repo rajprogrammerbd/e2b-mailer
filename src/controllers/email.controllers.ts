9c8be092a8da21752da908885fdffdf2ff4e1ec7
Raj Dutta <rd2249619@gmail.com>
2022-08-14 17:47:09 +0600

    add email route

---
+++ src/controllers/email.controllers.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import emailServices from '../services/email.services';
+import { RequestEmailSendBodyType } from '../utils/types';
+
+async function sendEmail(req: express.Request, res: express.Response) {
+    const { to, title, html, text } = req.body;
+    if ( to !== undefined && title !== undefined && (html !== undefined || text !== undefined) ) {
+
+        const body = { to, title } as RequestEmailSendBodyType;
+
+        (html !== undefined) ? body.html = html : null;
+        (text !== undefined) ? body.text = text : null;
+
+        try {
+            res.send(await emailServices.sendEmails(body));
+        } catch (err: any) {
+            throw new Error(err);
+        }
+    } else res.status(404).send('Request body is required');
+}
+
+export default {
+    sendEmail,
+};
---
+++ src/services/email.services.ts
@@ -0,0 +1,49 @@
+import { RequestEmailSendBodyType } from '../utils/types';
+import nodemailer from 'nodemailer';
+import logger from '../middlewares/winston';
+
+interface OptionsType {
+    from: string;
+    to: string;
+    subject: string;
+    html?: string;
+    text?: string;
+}
+
+function sendEmails(body: RequestEmailSendBodyType): Promise<any> {
+    const { to, title, html, text } = body;
+
+    return new Promise((resolve, reject) => {
+        const transporter = nodemailer.createTransport({
+            service : 'hotmail',
+            auth : {
+                user: 'rajkpi@outlook.com',
+                pass: 'Casino@123'
+            }
+        });
+
+        const options = {
+            from : 'rajkpi@outlook.com', 
+            to: to, 
+            subject: title,
+        } as OptionsType;
+
+        (html !== undefined) ? options.html = html : null;
+        (text !== undefined) ? options.text = text : null;
+    
+        const res = transporter.sendMail(options);
+        res.then(() => resolve(`Email has been successfully send to ${to}`)).catch(err => {
+            logger.error(err);
+            reject(`Failed to send an email to ${to}`);
+        });
+    });
+
+
+
+    return Promise.resolve({ message: 'Hello' });
+}
+  
+export default {
+  sendEmails,
+};
+  
