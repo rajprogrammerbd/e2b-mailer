c041eea65d9a94a299ac420541c575824e5bb617
Raj Dutta <rd2249619@gmail.com>
2022-08-16 14:14:51 +0600

    add test case

--- src/middlewares/isAccessible.ts
+++ src/middlewares/isAccessible.ts
@@ -2,11 +2,9 @@ import express from 'express';
 require('dotenv').config();
 
 function isAccessible(req: express.Request, res: express.Response, next: express.NextFunction) {
-        if (req.headers.authorization !== undefined) {
             if (req.headers.authorization === process.env.AUTHORIZATION_CODE) {
                 req.isAccessible = true;
-            } else req.isAccessible = false;
-        } else {
+            }else {
             req.isAccessible = false;
             res.status(401).json({ message: "You don't have permission to access it." });
         }
